d5d2ba6988ca1309f59c4d81e8693b9df4c2f8b2
tomer002 <78913951+tomer002@users.noreply.github.com>
2023-06-02 18:44:27 +0300

    basic profile picture taking.

--- app/src/main/java/com/example/groceries/SettingsActivity.java
+++ app/src/main/java/com/example/groceries/SettingsActivity.java
@@ -9,10 +9,12 @@ import static com.example.groceries.Keys.USERS;
 
 import android.app.Dialog;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -22,10 +24,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.storage.FirebaseStorage;
 
+import java.io.ByteArrayOutputStream;
+
 public class SettingsActivity extends AppCompatActivity {
 
     FirebaseStorage storage;
     String nickname;
+    boolean hasImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,7 +53,7 @@ public class SettingsActivity extends AppCompatActivity {
             String username = (String) snapshot.child(USERNAME).getValue();
             ((TextView) findViewById(R.id.nickname)).setText("nickname: " + nickname);
             ((TextView) findViewById(R.id.username)).setText("username: " + username);
-            boolean hasImage = (Boolean) snapshot.child(HAS_IMAGE).getValue();
+            hasImage = (Boolean) snapshot.child(HAS_IMAGE).getValue();
             if (hasImage) {
                 ImageView pfp = findViewById(R.id.pfp);
 
@@ -83,5 +88,35 @@ public class SettingsActivity extends AppCompatActivity {
             nicknameEditText.setSelection(nickname.length());
             nicknameEditText.requestFocus();
         });
+
+        findViewById(R.id.edit_pfp).setOnClickListener(v -> takePicture());
+    }
+
+    public void takePicture() {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);//go to camera
+        startActivityForResult(intent, 0);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 0) {
+            if (resultCode == RESULT_OK) {
+                Bitmap bitmap = (Bitmap) data.getExtras().get("data");
+                if (bitmap.getWidth() > 360 || bitmap.getHeight() > 360) {
+                    double ratio = 360 / Math.max(bitmap.getWidth(), bitmap.getHeight());
+                    bitmap = Bitmap.createScaledBitmap(bitmap, (int) (bitmap.getWidth() * ratio), (int) (bitmap.getHeight() * ratio), true);
+                }
+                ImageView pfp = findViewById(R.id.pfp);
+                pfp.setImageBitmap(bitmap);
+                ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                bitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos);
+                storage.getReference("profile_pictures").child(mAuth.getUid()).putBytes(baos.toByteArray());
+                if (!hasImage) {
+                    hasImage = true;
+                    database.getReference(USERS + "/" + mAuth.getUid() + "/" + HAS_IMAGE).setValue(true);
+                }
+            }
+        }
     }
 }
